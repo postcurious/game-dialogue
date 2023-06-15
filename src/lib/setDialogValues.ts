a5df223b81658290242b34f6d3c999b6eb60cc9f
Brennan Pilcher <brennan.pilcher@gmail.com>
2023-06-15 04:06:59 -0700

    Add debug logging to the chat message parsing logic

--- src/lib/setDialogValues.ts
+++ src/lib/setDialogValues.ts
@@ -10,6 +10,19 @@ const entries = Object.entries(variables as Variables)
  * @param {*} obj Object to replace variables within
  */
  export const setDialogValues = (obj: BotMessage | BotResponse | BotConditionalResponse) => {
+
+  if (window.debugChatMessages) {
+    console.log("attempting to set dialog values", obj)
+    try {
+      console.log("attempting to stringify", JSON.stringify(obj))
+  
+      console.log("attempting to stringify", JSON.parse(JSON.stringify(obj)))
+    } catch (e) {
+      console.log("ERROR ", e)
+    }
+  }
+
+
   return JSON.parse(JSON.stringify(obj), (_key, value) => {
     if (!Array.isArray(value)) return value
 
